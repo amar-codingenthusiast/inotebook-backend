6b9cb7e7f7b92ba960c2b909e2f19d18d9431fae
Amar Kumar <amar.codingenthusiast@gmail.com>
2024-04-28 17:09:41 +0530

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectToMongo = require("./db");
 
 connectToMongo();
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
 
-app.listen(port, () => {
-	console.log(`iNoteBook backend listening at ${port}`);
+app.listen(PORT, () => {
+	console.log(`iNoteBook backend listening at ${PORT}`);
 });
