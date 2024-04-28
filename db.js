2389c4b55d0531a51ddeffd24fd9a96574169fdf
Amar Kumar <amar.codingenthusiast@gmail.com>
2024-04-28 17:09:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://localhost:27017/iNoteBook";
-
+// const mongoURL = "mongodb://localhost:27017/iNoteBook";
+require("dotenv").config();
+const mongoName = process.env.MONGO_NAME;
+const mongoPass = process.env.MONGO_PASS;
+const mongoURL = `mongodb+srv://${mongoName}:${mongoPass}@inotebook.ifyelul.mongodb.net/iNoteBook?retryWrites=true&w=majority&appName=INOTEBOOK`;
 const connectToMongo = async () => {
 	try {
 		await mongoose.connect(mongoURL);
